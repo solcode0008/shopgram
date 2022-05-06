0c94831c400faf8a4ceb86ab97f2f8c2779289fa
k49977 <r.085.tk@i.softbank.jp>
2022-05-07 00:08:40 +0900

    ユーザー編集画面

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7b2497e95b58d492709fd3a0f7af745cc0d83e898fff2c7180455f3558c54298daffeff8866db536e882f492c7679a914b807938d49c8d2798089117f6b666ca'
