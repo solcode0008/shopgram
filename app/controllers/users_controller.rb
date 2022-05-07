99dc2b8174233adf4399872fc9e4ce11324c6d27
k49977 <r.085.tk@i.softbank.jp>
2022-05-07 11:48:04 +0900

    ユーザー画面のアクセス制限&フラッシュメッセージ

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,12 +11,18 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to users_path, alert: '不正なアクセスです' 
+    end
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
-    redirect_to user_path(@user)
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
   end
 
   private
