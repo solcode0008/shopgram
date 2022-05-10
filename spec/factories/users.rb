b96487ed26d64f127382b291fc5a6756af81da5d
k49977 <r.085.tk@i.softbank.jp>
2022-05-10 22:54:11 +0900

    User Model rspec実装

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,5 +2,6 @@ FactoryBot.define do
   factory :user do
     sequence(:username) { |n| "TEST_NAME#{n}"}
     sequence(:email) { |n| "TEST#{n}@example.com"}
+    sequence(:password) { |n| "TEST_PASS#{n}"}
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  it 'gets gate class depends on gate number'
+  let(:user) { create(:user) }
+
+  context 'ユーザー名のバリデーション' do
+    it 'ユーザー名が存在' do
+      user.username = 'Tony Stark'
+      expect(user).to be_valid
+    end
+    
+    it 'ユーザー名が空白' do
+      user.username = ' '
+      expect(user).to be_invalid
+      expect(user.errors[:username]).to include("can't be blank")
+    end
   end
 end
