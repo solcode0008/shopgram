7bb5afc2f90c0f1baf6036966e5a4208525d6d13
k49977 <r.085.tk@i.softbank.jp>
2022-05-10 11:42:01 +0900

    ログイン後のページを変更& プロフィール画像の大きさを固定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,9 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    shops_path # ログイン後に遷移するpathを設定
+  end
 
   private
   
