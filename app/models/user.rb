cec64584559ce80f90dadfa98cb8a4eb9c33f386
k49977 <r.085.tk@i.softbank.jp>
2022-06-09 07:10:55 +0900

    ゲストログイン機能の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
   has_many :shops, dependent: :destroy
 
   validates :username, presence: true 
+
+  def self.guest
+    find_or_create_by!(email: 'aaa@aaa.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+      user.username = 'サンプル'
+    end
+  end
 end
